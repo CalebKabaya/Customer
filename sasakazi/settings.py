c3bad56d19b2a32d0e53d706e9477287c08d9dac
Caleb <calebmutuku4@gmail.com>
2023-02-11 22:31:58 +0300

    added the allowed host

--- sasakazi/settings.py
+++ sasakazi/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-+oeopucobq-6z&trydi)7emse7b1fzd^!j)c6g88ixj8^k6yst
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['customer-production.up.railway.app']
+ALLOWED_HOSTS = ['customer-production.up.railway.app','127.0.0.1']
 
 
 # Application definition
