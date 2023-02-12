7734b770488a08e53b6bf4390c7a34ef921c5f93
Caleb <calebmutuku4@gmail.com>
2023-02-12 09:25:43 +0300

    added csrf settings

--- sasakazi/settings.py
+++ sasakazi/settings.py
@@ -153,4 +153,6 @@ STATICFILES_DIRS = [
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
+CSRF_TRUSTED_ORIGINS = ['https://customer-production.up.railway.app/']
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
